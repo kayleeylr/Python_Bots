14eae5ed653ebe0681e45dfb43e71702eefc694b
kayleeylr <823264073@qq.com>
2016-07-20 08:57:23 +0800

    基于udp客户端服务器：   UDP.py

---
+++ UDP.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+udpSerSock = socket(AF_INET, SOCK_DGRAM)
+udpSerSock.bind(ADDR)
+
+while True:
+    print 'waiting for message...'
+    data, addr = udpSerSock.recvfrom(BUFSIZ)
+    udpSerSock.sendto('[%s] %s' % (ctime(), data), addr)
+    print '...received from and returned to:', addr
+
+udpSerSock.close()
+
+
+
+
+#!/usr/bin/env python
+
+from socket import *
+
+HOST = 'localhost'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+udpCliSock = socket(AF_INET, SOCK_DGRAM)
+
+while True:
+    data = raw_input('> ')
+    if not data:
+        break
+    udpCliSock.sendto(data, ADDR)
+    data, ADDR = udpCliSock.recvfrom(BUFSIZ)
+    if not data:
+        break
+    print data
+
+udpCliSock.close()
